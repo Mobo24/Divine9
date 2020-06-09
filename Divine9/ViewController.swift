d13b880bf1aaeac850c9429cf90c44e85a215f4e
Mobo <princemoronfolu@gmail.com>
2020-06-08 23:27:52 -0500

    Initial Commit

---
+++ Divine9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divine9
+//
+//  Created by Mobolaji Moronfolu on 6/8/20.
+//  Copyright © 2020 Mobolaji Moronfolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Divine9Tests/Divine9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Divine9Tests.swift
+//  Divine9Tests
+//
+//  Created by Mobolaji Moronfolu on 6/8/20.
+//  Copyright © 2020 Mobolaji Moronfolu. All rights reserved.
+//
+
+import XCTest
+@testable import Divine9
+
+class Divine9Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
